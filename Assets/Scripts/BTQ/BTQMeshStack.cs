3d9574d103537d2cf296f63d7688343ffcd6e189
Dorian Cichoszewski <cichoszewski.dorian@gmail.com>
2024-06-04 22:54:39 +0200

    Improved mesh generator script

--- Assets/Scripts/BTQ/BTQMeshStack.cs
+++ Assets/Scripts/BTQ/BTQMeshStack.cs
@@ -3,84 +3,121 @@ using UnityEngine;
 
 namespace MagicCombat
 {
-    [RequireComponent(typeof(CanvasRenderer))]
-    public class MeshStack : MonoBehaviour
-    {
-        [SerializeField]
-        private Vector2 size = new(1,1);
-        
-        [SerializeField]
-        private int count = 1;
+	[RequireComponent(typeof(CanvasRenderer))]
+	public class MeshStack : MonoBehaviour
+	{
+		[SerializeField]
+		private int count = 1;
 
-        [SerializeField]
-        private float distance = 0.5f;
+		[SerializeField]
+		private float distance = 0.5f;
 
-        [PreviewField(Height = 200)]
-        [ShowInInspector]
-        private Mesh mesh;
-        
-        [Button]
-        public void Test()
-        {
-            var rectTransform = GetComponent<RectTransform>();
-            mesh = new Mesh();
+		[SerializeField]
+		private Material material;
 
-            size = rectTransform.sizeDelta;
+		[SerializeField]
+		private Texture texture;
 
-            var pivot = rectTransform.pivot;
+		[SerializeField]
+		private Texture alphaTexture;
 
-            Vector3[] vertices = new Vector3[4 * count];
-            Vector3[] normals = new Vector3[vertices.Length];
-            Vector2[] uv = new Vector2[vertices.Length];
-            Color[] colors = new Color[vertices.Length];
-            int[] tris = new int[6 * count];
+		[SerializeField]
+		[Range(0f, 1f)]
+		private float alpha = 1;
 
-            float left = -pivot.x * size.x;
-            float right = (1 - pivot.x) * size.x;
-            float bottom = -pivot.y * size.y;
-            float top = (1 - pivot.y) * size.y;
-            
+		[SerializeField]
+		private Color color = Color.white;
 
-            for (int i = 0; i < count; i++)
-            {
-                vertices[4 * i + 0] = new Vector3(left, bottom, i * distance);
-                vertices[4 * i + 1] = new Vector3(right, bottom, i * distance);
-                vertices[4 * i + 2] = new Vector3(left, top, i * distance);
-                vertices[4 * i + 3] = new Vector3(right, top, i * distance);
-                
-                normals[4 * i + 0] = Vector3.forward;
-                normals[4 * i + 1] = Vector3.forward;
-                normals[4 * i + 2] = Vector3.forward;
-                normals[4 * i + 3] = Vector3.forward;
-                
-                uv[4 * i + 0] = new Vector2(0, 0);
-                uv[4 * i + 1] = new Vector2(1, 0);
-                uv[4 * i + 2] = new Vector2(0, 1);
-                uv[4 * i + 3] = new Vector2(1, 1);
+		[PreviewField(Height = 200)]
+		[ShowInInspector]
+		private Mesh mesh;
 
-                float vertexTint = i / (float)(count - 1);
-                var color = new Color(vertexTint, vertexTint, vertexTint, 1);
+		private RectTransform rectTransform;
+		private CanvasRenderer canvasRenderer;
 
-                colors[4 * i + 0] = color;
-                colors[4 * i + 1] = color;
-                colors[4 * i + 2] = color;
-                colors[4 * i + 3] = color;
-                
-                tris[6 * i + 0] = 4 * i + 0;
-                tris[6 * i + 1] = 4 * i + 1;
-                tris[6 * i + 2] = 4 * i + 2;
-                tris[6 * i + 3] = 4 * i + 2;
-                tris[6 * i + 4] = 4 * i + 1;
-                tris[6 * i + 5] = 4 * i + 3;
-            }
-            
-            mesh.vertices = vertices;
-            mesh.normals = normals;
-            mesh.uv = uv;
-            mesh.triangles = tris;
-            mesh.colors = colors;
-            
-            GetComponent<CanvasRenderer>().SetMesh(mesh);
-        }
-    }
+		public CanvasRenderer CanvasRenderer => canvasRenderer ??= GetComponent<CanvasRenderer>();
+
+		public RectTransform RectTransform => rectTransform ??= GetComponent<RectTransform>();
+
+		private void OnEnable()
+		{
+			GenerateMesh();
+		}
+
+		private void OnValidate()
+		{
+			GenerateMesh();
+		}
+
+		// private void OnRectTransformDimensionsChange()
+		// {
+		//     GenerateMesh();
+		// }
+
+		public void GenerateMesh()
+		{
+			mesh = new Mesh();
+
+			Vector2 size = RectTransform.sizeDelta;
+
+			var pivot = RectTransform.pivot;
+
+			Vector3[] vertices = new Vector3[4 * count];
+			Vector3[] normals = new Vector3[vertices.Length];
+			Vector2[] uv = new Vector2[vertices.Length];
+			Color[] colors = new Color[vertices.Length];
+			int[] tris = new int[6 * count];
+
+			float left = -pivot.x * size.x;
+			float right = (1 - pivot.x) * size.x;
+			float bottom = -pivot.y * size.y;
+			float top = (1 - pivot.y) * size.y;
+
+			for (int i = 0; i < count; i++)
+			{
+				vertices[4 * i + 0] = new Vector3(left, bottom, i * distance);
+				vertices[4 * i + 1] = new Vector3(right, bottom, i * distance);
+				vertices[4 * i + 2] = new Vector3(left, top, i * distance);
+				vertices[4 * i + 3] = new Vector3(right, top, i * distance);
+
+				normals[4 * i + 0] = Vector3.forward;
+				normals[4 * i + 1] = Vector3.forward;
+				normals[4 * i + 2] = Vector3.forward;
+				normals[4 * i + 3] = Vector3.forward;
+
+				uv[4 * i + 0] = new Vector2(0, 0);
+				uv[4 * i + 1] = new Vector2(1, 0);
+				uv[4 * i + 2] = new Vector2(0, 1);
+				uv[4 * i + 3] = new Vector2(1, 1);
+
+				float vertexTint = i / (float)(count - 1);
+				var color = new Color(vertexTint, vertexTint, vertexTint, 1);
+
+				colors[4 * i + 0] = color;
+				colors[4 * i + 1] = color;
+				colors[4 * i + 2] = color;
+				colors[4 * i + 3] = color;
+
+				tris[6 * i + 0] = 4 * i + 0;
+				tris[6 * i + 1] = 4 * i + 1;
+				tris[6 * i + 2] = 4 * i + 2;
+				tris[6 * i + 3] = 4 * i + 2;
+				tris[6 * i + 4] = 4 * i + 1;
+				tris[6 * i + 5] = 4 * i + 3;
+			}
+
+			mesh.vertices = vertices;
+			mesh.normals = normals;
+			mesh.uv = uv;
+			mesh.triangles = tris;
+			mesh.colors = colors;
+
+			CanvasRenderer.SetMesh(mesh);
+			CanvasRenderer.SetMaterial(material ?? Canvas.GetDefaultCanvasMaterial(), null);
+			CanvasRenderer.SetTexture(texture);
+			CanvasRenderer.SetColor(color);
+			CanvasRenderer.SetAlphaTexture(alphaTexture);
+			CanvasRenderer.SetAlpha(alpha);
+		}
+	}
 }
